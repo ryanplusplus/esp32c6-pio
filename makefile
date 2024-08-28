dbcfc80ba5fec02f2f84e155f9e7c9ae9f7cf09d
Ryan Hartlage <ryanplusplus@gmail.com>
2024-08-27 21:21:00 -0400

    Switch to community fork

---
+++ makefile
@@ -0,0 +1,20 @@
+.PHONY: all
+all:
+	@pio run
+
+.PHONY: upload
+upload:
+	@pio run -t upload
+
+.PHONY: run
+run: upload
+	@sleep 1.5
+	@pio device monitor
+
+.PHONY: monitor
+monitor:
+	@pio device monitor
+
+.PHONY: clean
+clean:
+	@pio run -t clean
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,42 @@
+#include <Adafruit_NeoPixel.h>
 #include <Arduino.h>
+
+enum {
+  pin = 0,
+  pixel_count = 60,
+  step = 1
+};
+
+static Adafruit_NeoPixel pixels(pixel_count, pin, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
-  pinMode(15, OUTPUT);
+  pixels.begin();
+  pixels.clear();
 }
+
+typedef struct {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+} Color;
+
+static constexpr Color colors[] = {
+  { 1, 0, 0 },
+  { 1, 1, 0 },
+  { 0, 1, 0 },
+  { 0, 1, 1 },
+  { 0, 0, 1 },
+  { 1, 0, 1 }
+};
 
 void loop()
 {
-  digitalWrite(15, HIGH);
-  delay(500);
-  digitalWrite(15, LOW);
-  delay(500);
+  for(size_t color = 0; color < sizeof(colors) / sizeof(colors[0]); color++) {
+    for(int pixel = 0; pixel < pixel_count; pixel++) {
+      pixels.setPixelColor(pixel, pixels.Color(colors[color].r, colors[color].g, colors[color].b));
+      pixels.show();
+      delay(step);
+    }
+  }
 }
